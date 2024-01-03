c77ebe7cac63f6f43cc2aa8572f575b335d1d366
Alexandre-FP <alexandrefernandes.132132@gmail.com>
2024-01-03 02:03:22 -0300

    separando_arquivos

--- src/server.js
+++ src/server.js
@@ -2,7 +2,4 @@ import app from "./app";
 
 const PORT = process.env.PORT_DESENVOLVIMENTO;
 
-app.listen(PORT, () => console.log(`✅ Servidor rodando na porta ${PORT}`));  
-
-
-  
+app.listen(PORT, () => console.log(`✅ Servidor rodando na porta ${PORT}`));
